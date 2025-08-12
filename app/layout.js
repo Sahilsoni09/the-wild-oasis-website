e2b7e86f65afa5be61c14ba7df40fe4d64228500
Sahilsoni09 <Sahilsoni7609@gmail.com>
2025-08-12 17:58:03 +0530

    eat(fonts): optimize font loading with Next.js self-hosting - Integrated Google font  Josefin Sans using ext/font/google   • Added subsets: ['latin'] for smaller bundle size   • Used display: 'swap' for better UX and reduced layout shift (CLS) - Applied font globally by adding josefin.className to <body> in RootLayout - Removed need for external Google Fonts CDN requests → improved performance & GDPR compliance

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,12 @@
 import Logo from "./_components/Logo";
 import Navigation from "./_components/Navigation";
 import "@/app/_styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
 
+const Josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
 export const metadata = {
   // title: "The Wild Oasis",
   title: {
@@ -14,7 +19,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
+      <body
+        className={`${Josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+      >
         <header>
           <Logo />
           <Navigation />
