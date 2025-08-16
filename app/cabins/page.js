4e3f3711f8560a7994c6992de8ff7c4b7d930f8e
Sahilsoni09 <Sahilsoni7609@gmail.com>
2025-08-16 17:56:23 +0530

    feat(cabins): add per-route loading state with custom spinner and message

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,12 +1,14 @@
 import Navigation from "@/app/_components/Navigation";
 import CabinCard from "../_components/CabinCard";
+import { getCabins } from "../_lib/data-service";
 
 export const metadata = {
   title: "Cabins",
 };
-export default function Page() {
+export default async function Page() {
   // CHANGE
-  const cabins = [];
+  const cabins = await getCabins();
+  console.log("Cabins loaded:", cabins);
 
   return (
     <div>
