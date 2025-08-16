e2c8e1e3f7b5e7b7210b9ed5c7c34e813270a692
Sahilsoni09 <Sahilsoni7609@gmail.com>
2025-08-16 22:21:33 +0530

    feat: implement granular streaming for CabinList with Suspense - Moved cabin list fetching & rendering into a new server component (CabinList.js) - Wrapped CabinList with React.Suspense in page.js - Added <Spinner /> fallback to show loader only for cabin list - Compared with loading.js → Suspense enables partial UI rendering

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,14 +1,12 @@
-import Navigation from "@/app/_components/Navigation";
-import CabinCard from "../_components/CabinCard";
-import { getCabins } from "../_lib/data-service";
+import { Suspense } from "react";
+import CabinList from "../_components/CabinList";
+import Spinner from "../_components/Spinner";
 
 export const metadata = {
   title: "Cabins",
 };
 export default async function Page() {
   // CHANGE
-  const cabins = await getCabins();
-  console.log("Cabins loaded:", cabins);
 
   return (
     <div>
@@ -23,14 +21,9 @@ export default async function Page() {
         home away from home. The perfect spot for a peaceful, calm vacation.
         Welcome to paradise.
       </p>
-
-      {cabins.length > 0 && (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-          {cabins.map((cabin) => (
-            <CabinCard cabin={cabin} key={cabin.id} />
-          ))}
-        </div>
-      )}
+      <Suspense fallback={<Spinner />}>
+        <CabinList />
+      </Suspense>
     </div>
   );
 }
